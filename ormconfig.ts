daaab686cd2a1488f7106a88eef28fe9eedb6573
Kensuke Ikunaga <iku12phycho@gmail.com>
2022-01-11 17:46:21 +0000

    edit ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -6,10 +6,7 @@ const port = process.env.DB_PORT || '3300';
 const ormconfig: ConnectionOptions = {
   type: "mysql",
   host: process.env.DB_HOST,
-  port: parseInt(port, 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: `${process.env.DB_URL}`,
   synchronize: false,
   logging: true,
   entities: ["entities/**/*.ts"],
