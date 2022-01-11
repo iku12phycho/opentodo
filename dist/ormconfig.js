c6250ff1b3038ece64987534ff349467a888a2cf
Kensuke Ikunaga <iku12phycho@gmail.com>
2022-01-11 18:34:40 +0000

    edit migration config

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -12,9 +12,9 @@ const ormconfig = {
     url: `${process.env.DB_URL}`,
     synchronize: false,
     logging: true,
-    entities: ["entities/**/*.ts"],
-    migrations: ["db/migrations/**/*.ts"],
-    subscribers: ["db/subscriber/**/*.ts"],
+    entities: ["entities/**/*.js"],
+    migrations: ["db/migrations/**/*.js"],
+    subscribers: ["db/subscriber/**/*.js"],
     cli: {
         entitiesDir: "entities",
         migrationsDir: "db/migrations",
